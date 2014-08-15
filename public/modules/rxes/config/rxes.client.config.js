cfed89404b593f74f18542d2e1daf42f6cf07bb3
Max Johansen <macsj200@gmail.com>
2014-08-15 03:30:22 -0600

    Add search menu option

--- public/modules/rxes/config/rxes.client.config.js
+++ public/modules/rxes/config/rxes.client.config.js
@@ -5,6 +5,7 @@ angular.module('rxes').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
 		Menus.addMenuItem('topbar', 'Rxes', 'rxes', 'dropdown', '/rxes(/create)?');
+        Menus.addSubMenuItem('topbar', 'rxes', 'Search Rx', 'rxes/search');
 		Menus.addSubMenuItem('topbar', 'rxes', 'List Rxes', 'rxes');
 		Menus.addSubMenuItem('topbar', 'rxes', 'New Rx', 'rxes/create');
 	}
