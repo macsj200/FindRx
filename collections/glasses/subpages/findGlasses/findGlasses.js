c8af5083fd870e9ce57a4b8a42e852beae2609b4
Max Johansen <maxjohansen@berkeley.edu>
2015-08-27 21:29:29 -0700

    stuff

--- collections/glasses/subpages/findGlasses/findGlasses.js
+++ collections/glasses/subpages/findGlasses/findGlasses.js
@@ -1,29 +1,40 @@
 Glasses.paginations = [];
 
 Glasses.paginations.push(new Meteor.Pagination(Glasses, {
-       infinite: true,
-       itemTemplate: 'glassInFindList',
-       templateName: "glasses",
-       sort: {
-           createdAt: -1
-       },
-       availableSettings: {
-           sort: true,
-           filter: true
-       },
-       fastRender: true
+  infinite: true,
+  itemTemplate: 'glassInFindList',
+  templateName: "glasses",
+  sort: {
+    createdAt: -1
+  },
+  availableSettings: {
+    sort: true,
+    filter: true
+  },
+  fastRender: true
 }));
 
 if (Meteor.isClient) {
 
-    Template.findGlasses.onCreated(function() {
+  AutoForm.hooks({
+    findGlass:{
+      before:{
+        update:function(doc){
+          console.log(Glasses.find(doc.$set).fetch());
+          this.result(false);
+        }
+      }
+    }
+  });
 
-      Tracker.autorun(function(){
+  Template.findGlasses.onCreated(function() {
 
-        Glasses.paginations[0].set({
-          sort: Template.currentData().sort || {},
-          filter: Template.currentData().filter || {}
-        });
+    Tracker.autorun(function(){
+
+      Glasses.paginations[0].set({
+        sort: Template.currentData().sort || {},
+        filter: Template.currentData().filter || {}
+      });
     });
   });
 
